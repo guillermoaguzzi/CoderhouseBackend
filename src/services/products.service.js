1f4b49861f93312579df3a78249bfe08b20025bc
GuillermoAguzzi <gpaguzzi@gmail.com>
2023-10-23 22:08:55 +1000

    Dao for products debugged

--- src/services/products.service.js
+++ src/services/products.service.js
@@ -1,4 +1,4 @@
-const ProductDao = require("../daos/products.Dao");
+const ProductDao = require("../daos/products.dao");
 const { generateProducts } = require("../utils/mocks/generate.products");
 const MailingService = require("./mailing.service");
 const { EnumErrors, HttpResponse } = require("../middleware/errors.middleware");
